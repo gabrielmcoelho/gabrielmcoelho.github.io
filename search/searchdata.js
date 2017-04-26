587f8cd73a9bfae103d0165475fdd479a2a73619
gabrielmcoelho <gabriel_mcoelho@hotmail.com>
2017-04-25 23:44:42 -0300

    Add files via upload

---
+++ search/searchdata.js
@@ -0,0 +1,21 @@
+var indexSectionsWithContent =
+{
+  0: "aginoprstv",
+  1: "pr",
+  2: "aginoprstv"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "functions"
+};
+
+var indexSectionLabels =
+{
+  0: "Tudo",
+  1: "Classes",
+  2: "Funções"
+};
+
